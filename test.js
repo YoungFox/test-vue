a17f4ba02ac46f5b48c241edc038c01a5566d100
yangwenliang <yang6233562@gmail.com>
2017-03-01 11:04:53 +0800

    lifecycle hooks

--- test.js
+++ test.js
@@ -1,7 +1,10 @@
 // 每个 Vue 实例都会代理其 data 对象里所有的属性：
 var data = { a: 1 }
 var vm = new Vue({
-  data: data
+  data: data,
+  created: function (){
+  	console.log('a is:' + this.a);
+  }
 })
 console.log(vm.a === data.a);
 // -> true
@@ -13,3 +16,18 @@ console.log(data.a);
 // 反之亦然
 data.a = 3;
 console.log(vm.a);
+
+var data1 = {a: 1};
+
+var vm1 = new Vue({
+	el: '#example',
+	data: data1
+});
+
+console.log(vm1.$data === data1);
+console.log(vm1.$el === document.getElementById('example'));
+
+vm1.$watch('a',function(newVal, oldVal){
+	console.log(newVal);
+	console.log(oldVal);
+})
