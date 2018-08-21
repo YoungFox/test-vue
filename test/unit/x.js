a13fac7e5d577c99fb298ffbf2a67f96c64257d0
yangwenliang <yang6233562@gmail.com>
2018-08-21 17:50:11 +0800

    ignore

--- test/unit/x.js
+++ test/unit/x.js
@@ -4,8 +4,9 @@ describe('第一个测试', function () {
 
     it('不用new实例化', function () {
         // expect(true).not.toBe(true);
-        expect(Vue() instanceof Vue).toBe(true);
-
+        try {Vue()}catch(e){
+            expect(`Vue is a constructor and should be called with the 'new' keyword`).toHaveBeenWarned();
+        }
     });
 
     it('用new实例化', function () {
